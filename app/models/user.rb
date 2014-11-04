fc8d869f0b5d9f856421c571dd7571208aead4ff
DarryQueen <slimfrog@gmail.com>
2014-11-03 17:58:36 -0800

    Creates a new user if one cannot be found with given email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,17 @@ class User < ActiveRecord::Base
   def initialize_fields
     self.wallet = Wallet.new(points: DareKickstarter::Application.config.starting_points)
   end
+
+  # Static methods:
+  def self.find_by_email(email)
+    if email.nil?
+      return nil
+    end
+    self.where(:email => email).first_or_create(self.new_user_params)
+  end
+
+  def self.new_user_params
+    temp_password = (0...8).map { (65 + rand(26)).chr }.join
+    { :password => temp_password, :password_confirmation => temp_password }
+  end
 end
