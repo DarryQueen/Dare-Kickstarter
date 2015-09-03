4b3148f5e20e515792307b62aa3493c2da6934a1
DarryQueen <slimfrog@gmail.com>
2015-09-03 14:43:22 -0700

    Basic OAuth setup.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable
+
   has_one :wallet
   has_many :funds
 
   after_initialize :initialize_fields
+
+  TEMP_EMAIL_PREFIX = 'change@me'
 
   def initialize_fields
     self.wallet = Wallet.new(points: DareKickstarter::Application.config.starting_points)
@@ -23,5 +24,34 @@ class User < ActiveRecord::Base
   def self.new_user_params
     temp_password = (0...8).map { (65 + rand(26)).chr }.join
     { :password => temp_password, :password_confirmation => temp_password }
+  end
+
+  def self.find_for_oauth(auth, signed_in_resource = nil)
+    identity = Identity.find_for_oauth(auth)
+
+    user = signed_in_resource ? signed_in_resource : identity.user
+
+    if user.nil?
+      email = auth.info.email
+      user = User.where(:email => email).first if email
+
+      # Create the user if it's a new registration.
+      if user.nil?
+        user = User.new(
+          :email => email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
+          :password => Devise.friendly_token[0, 20],
+          :name => auth.info.name
+        )
+        user.skip_confirmation! if user.respond_to?(:skip_confirmation)
+        user.save!
+      end
+    end
+
+    # Associate the identity with the user if needed:
+    if identity.user != user
+      identity.user = user
+      identity.save!
+    end
+    user
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141102032931) do
+ActiveRecord::Schema.define(version: 20150903212419) do
 
   create_table "dares", force: true do |t|
     t.string   "title"
@@ -30,6 +30,16 @@ ActiveRecord::Schema.define(version: 20141102032931) do
     t.datetime "updated_at"
     t.integer  "user_id"
   end
+
+  create_table "identities", force: true do |t|
+    t.integer  "user_id"
+    t.string   "provider"
+    t.string   "uid"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "identities", ["user_id"], name: "index_identities_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
